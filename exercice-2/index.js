ab9ceb7b54a3b0126edd76df9fb21bcea78398f2
smimoon <simonlaroche@orange.fr>
2026-01-16 09:43:23 +0000

    Save end exercice-2

--- exercice-2/index.js
+++ exercice-2/index.js
@@ -1,5 +1,8 @@
 // vos imports ici
-
+import {numbers} from "./numbers.js";
 window.addEventListener("DOMContentLoaded", () => {
    // votre code ici
+   for(let number of numbers) {
+       console.log(number);
+   }
 });
